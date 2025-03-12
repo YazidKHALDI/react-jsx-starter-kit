6981630caf81fea11205209219e1d40aa1993cec
Yazid KHALDI <khaldi.yazid@gmail.com>
2025-03-12 03:46:34 +0000

    Converted Laravel React Starter Kit to JSX version

--- resources/js/pages/settings/appearance.jsx
+++ resources/js/pages/settings/appearance.jsx
@@ -2,13 +2,12 @@ import { Head } from '@inertiajs/react';
 
 import AppearanceTabs from '@/components/appearance-tabs';
 import HeadingSmall from '@/components/heading-small';
-import { type BreadcrumbItem } from '@/types';
 
 import AppLayout from '@/layouts/app-layout';
 import SettingsLayout from '@/layouts/settings/layout';
 import { edit as editAppearance } from '@/routes/appearance';
 
-const breadcrumbs: BreadcrumbItem[] = [
+const breadcrumbs = [
     {
         title: 'Appearance settings',
         href: editAppearance().url,
